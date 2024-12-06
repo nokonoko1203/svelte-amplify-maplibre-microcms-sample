998a5dfd62fb70ea0053a36ca755d2cbac4cbe0f
nokonoko1203 <healtheworld.1203@gmail.com>
2024-12-06 21:52:54 +0900

    log

--- src/lib/cms/cms-client.server.ts
+++ src/lib/cms/cms-client.server.ts
@@ -5,6 +5,8 @@ import type { Post } from "./types";
 
 let client: ReturnType<typeof createClient> | undefined = undefined;
 const getClient = () => {
+	console.log("env.SERVICE_DOMAIN", env.SERVICE_DOMAIN);
+	console.log("env.MICROCMS_API_KEY", env.MICROCMS_API_KEY);
 	if (client) return client;
 	client = createClient({
 		serviceDomain: env.SERVICE_DOMAIN ?? "",
